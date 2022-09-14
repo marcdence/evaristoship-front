d40ca90552b200f97ab62a8fa46e868fea6f8b37
marcdence <mddalogdog14@gmail.com>
2022-09-14 15:41:06 +0800

    update vessel

--- components/schedules/ScheduleContainer.vue
+++ components/schedules/ScheduleContainer.vue
@@ -225,7 +225,7 @@
                   <b>Return</b>
                 </div>
                 <div class="pa-5" v-if="book.return_price != 0">
-                  <div class="text-h6 pb-5"><b>FastCat M19</b></div>
+                  <div class="text-h6 pb-5"><b>M/V Nathan/Sean Ferry Fastcraft</b></div>
                   <div class="text-h6">
                     {{ $route.query.to }} - {{ $route.query.from }}
                   </div>
@@ -399,7 +399,7 @@
                   <b>Departure</b>
                 </div>
                 <div class="pa-5" v-if="book.departure_price != 0">
-                  <div class="text-h6 pb-5"><b>FastCat M19</b></div>
+                  <div class="text-h6 pb-5"><b>M/V Nathan/Sean Ferry Fastcraft</b></div>
                   <div class="text-h6">
                     {{ $route.query.from }} - {{ $route.query.to }}
                   </div>
@@ -454,7 +454,7 @@
                   <b>Return</b>
                 </div>
                 <div class="pa-5" v-if="book.return_price != 0">
-                  <div class="text-h6 pb-5"><b>FastCat M19</b></div>
+                  <div class="text-h6 pb-5"><b>M/V Nathan/Sean Ferry Fastcraft</b></div>
                   <div class="text-h6">
                     {{ $route.query.to }} - {{ $route.query.from }}
                   </div>
